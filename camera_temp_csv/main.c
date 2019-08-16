c181de4d90997a000bd4d395405c65b349649c93
brianlesllie <47663598+brianlesllie@users.noreply.github.com>
2019-08-16 11:27:36 -0300

    Update and rename requisitos to main.c

---
+++ camera_temp_csv/main.c
@@ -0,0 +1,32 @@
+/** Programa que exibe na tela dados formatados
+ * de um arquivo CSV conforme o padrÃ£o: 
+ *         Series;Value;Time
+ *         <int>;<float>;<string> 
+ *         <int>;<float>;<string>
+ */
+
+#include <stdio.h>
+
+int main() {
+
+	int amostra;
+	float temperatura;
+	char data[64];
+
+	FILE *fp = fopen("camera_temp.csv", "r");
+
+	if (fp==NULL) {
+		perror("Erro em main: fopen");
+		exit(-1);
+	}
+
+	/* Ignora primeira linha */
+	fgets(data, 64, fp);
+
+	while (fscanf(fp, "%d,%f,%63[^\n]\n", &amostra, &temperatura, data)==3) {
+			printf("%d  --  %f  --- %s\n", amostra, temperatura, data);
+	}
+
+	fclose (fp);
+	return 0;
+}
