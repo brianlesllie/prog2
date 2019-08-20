cd27c6b1adbd80edfa6aeed5314d5b4827143545
brianlesllie <47663598+brianlesllie@users.noreply.github.com>
2019-08-20 11:18:23 -0300

    Update main.c

--- camera_temp_csv/main.c
+++ camera_temp_csv/main.c
@@ -1,32 +1,63 @@
 /** Programa que exibe na tela dados formatados
- * de um arquivo CSV conforme o padrÃ£o: 
- *         Series;Value;Time
- *         <int>;<float>;<string> 
- *         <int>;<float>;<string>
- */
-
+  * de um arquivo CSV conforme o padrão: 
+  *         Series;Value;Time
+  *         <int>;<float>;<string> 
+  *         <int>;<float>;<string>
+  */ 
+  
 #include <stdio.h>
 
-int main() {
 
-	int amostra;
-	float temperatura;
-	char data[64];
+struct dado_t {
+    int amostra;
+    float temperatura;
+    char tempo[64];
+}
 
-	FILE *fp = fopen("camera_temp.csv", "r");
 
-	if (fp==NULL) {
-		perror("Erro em main: fopen");
-		exit(-1);
-	}
+int main(){
+    int i;
+    int n_linhas = 0;
+    char *info;
+    /* Ponteiro para alocação dinâmica dos dados */
+    struct dado_t *dados = NULL;
 
-	/* Ignora primeira linha */
-	fgets(data, 63, fp);
+    FILE *fp = fopen("camera_temp.csv","r");
+    
+    if (!fp){
+        perror("main");
+        exit(-1);        
+    }    
+    
+    /* Ignora primeira linha */
+    fgets(info,64, fp);
 
-	while (fscanf(fp, "%d,%f,%63[^\n]\n", &amostra, &temperatura, data)==3) {
-			printf("%d  --  %f  --- %s\n", amostra, temperatura, data);
-	}
+    /* Contar todas as linhas: use while e fgets() */
+    while ( fgets(info, 64, fp) != NULL){
+       n_linhas++;
+    }
 
-	fclose (fp);
-	return 0;
+    /* Aloque memória:
+    dados = malloc(sizeof(struct dado_t) * n_linhas); */
+    
+    rewind(fp);
+     /* Ignora primeira linha */
+    fgets(info,64, fp);
+    
+    /* Modifique aqui para colocar os dados no vetor 
+     * de estruturas alocado  dinamicamente *
+     * ex: dados[i].amostra = amostra; */
+     
+    while (fscanf (fp, "%d,%f,%63[^\n]", &amostra, &temperatura, data) == 3){
+       printf("%d, %f, %s\n", amostra, temperatura, data);
+    }
+
+
+    /* Imprima os dados:*/
+    for (i=0; i < n_linhas; i++){
+        //printf( "...", dados[i].amostra, ... );
+    }
+
+
+    fclose(fp);
 }
