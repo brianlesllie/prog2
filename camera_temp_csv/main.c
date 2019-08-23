fc74cf22c06cb21f15182855a7118ec59dbca76d
brianlesllie <47663598+brianlesllie@users.noreply.github.com>
2019-08-23 11:26:30 -0300

    Update main.c

--- camera_temp_csv/main.c
+++ camera_temp_csv/main.c
@@ -1,34 +1,34 @@
 /** Programa que exibe na tela dados formatados
-  * de um arquivo CSV conforme o padrão: 
+  * de um arquivo CSV conforme o padrão:
   *         Series;Value;Time
-  *         <int>;<float>;<string> 
   *         <int>;<float>;<string>
-  */ 
-  
-#include <stdio.h>
+  *         <int>;<float>;<string>
+  */
 
+#include <stdio.h>
+#include <stdlib.h>
 
 struct dado_t {
     int amostra;
     float temperatura;
     char tempo[64];
-}
+};
 
 
 int main(){
-    int i;
+    int i=0;
     int n_linhas = 0;
-    char *info;
+    char info[64];
     /* Ponteiro para alocação dinâmica dos dados */
     struct dado_t *dados = NULL;
 
     FILE *fp = fopen("camera_temp.csv","r");
-    
+
     if (!fp){
         perror("main");
-        exit(-1);        
-    }    
-    
+        exit(-1);
+    }
+
     /* Ignora primeira linha */
     fgets(info,64, fp);
 
@@ -37,25 +37,26 @@ int main(){
        n_linhas++;
     }
 
-    /* Aloque memória:
-    dados = malloc(sizeof(struct dado_t) * n_linhas); */
-    
+    /* Aloque memória: */
+    dados = malloc(sizeof(struct dado_t) * n_linhas);
+
     rewind(fp);
      /* Ignora primeira linha */
     fgets(info,64, fp);
-    
-    /* Modifique aqui para colocar os dados no vetor 
+
+    /* Modifique aqui para colocar os dados no vetor
      * de estruturas alocado  dinamicamente *
      * ex: dados[i].amostra = amostra; */
-     
-    while (fscanf (fp, "%d,%f,%63[^\n]", &amostra, &temperatura, data) == 3){
-       printf("%d, %f, %s\n", amostra, temperatura, data);
+
+    while (fscanf (fp, "%d,%f,%63[^\n]", &dados[i].amostra, &dados[i].temperatura, dados[i].tempo) == 3){
+       //printf("%d, %f, %s\n", dados[i].amostra, dados[i].temperatura, dados[i].tempo);
+       i++;
     }
 
 
     /* Imprima os dados:*/
     for (i=0; i < n_linhas; i++){
-        //printf( "...", dados[i].amostra, ... );
+    	printf("%d, %f, %s\n", dados[i].amostra, dados[i].temperatura, dados[i].tempo);
     }
 
 
